548617a166fa59f9b55503743d38fe1fa1aa54ec
Eugene Babichev <eugene.babichev@rokt.com>
2024-07-09 17:44:06 +1000

    Set default shared cache TTL

--- pkg/utils/sharedcache/sharedcache.go
+++ pkg/utils/sharedcache/sharedcache.go
@@ -2,18 +2,20 @@ package sharedcache
 
 import (
 	"sync"
+	"time"
 
 	"github.com/patrickmn/go-cache"
 )
 
-// private variable to hold the instance
 var sharedCache *cache.Cache
 var once sync.Once
+
+const DefaultSharedCacheTTL time.Duration = time.Hour * 24
 
 // SharedCache provides a global point of access to the Cache instance
 func SharedCache() *cache.Cache {
 	once.Do(func() {
-		sharedCache = cache.New(cache.NoExpiration, cache.NoExpiration)
+		sharedCache = cache.New(DefaultSharedCacheTTL, time.Hour)
 	})
 	return sharedCache
 }
